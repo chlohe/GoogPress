4be882da8e0a4379d4faa0907dc3279d9e50a8ce
Chloe <chloe.he.21@ucl.ac.uk>
2017-01-21 20:50:48 +0000

    Some basic client-side stuff

---
+++ Demo/GoogPress.js
@@ -0,0 +1,34 @@
+var hostUrl = "";
+
+function loadPost (name, container){
+
+    $.post(
+        hostUrl,
+        {type : "page",
+         id : name,
+         asHtml : true
+        },
+        function(data) {
+            //set the container html
+            container.html(data);
+        });
+
+}
+
+//Initialise Everything
+function Init(url){
+
+    hostUrl = url;
+    
+    //Find everything that want's to be googpress-ed
+    var containers = $('div').filter(function() {
+        return $(this).data('googpress') != undefined;
+    });
+
+    //googpress them
+    containers.each(function () {
+        loadPost ($(this).data("googpress"), $(this));
+    });
+
+
+}
