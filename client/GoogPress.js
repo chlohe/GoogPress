f959f1c7b742cee8fd33b99693bb6e2a38810d31
Stewart McGown <stewartcallummcgown@googlemail.com>
2017-01-22 23:15:50 +0000

    Added extra reload functionality

--- client/GoogPress.js
+++ client/GoogPress.js
@@ -17,6 +17,8 @@ function loadPost (name, container){
 //Initialise Everything
 function Init(url){
 
+    $(".overlay").fadeIn();
+    
     hostUrl = url;
     
     //Find everything that want's to be googpress-ed
@@ -32,5 +34,5 @@ function Init(url){
 }
 
 $(document).ajaxStop(function() {
-  $(".overlay").fadeOut();
+  $(".overlay").removeClass('slideInUp').addClass('slideOutDown');
 });
